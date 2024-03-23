34655e1d52d28aa5e8db0ea7de529604199ac1f8
Niti <earthza6263@gmail.com>
2024-03-23 11:22:53 +0700

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "cqphjwakpkovcvrouaoz.supabase.co",
+          port: "",
+          pathname: "/storage/v1/object/public/Images/**",
+        },
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "daisyui.com",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "lh3.googleusercontent.com",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
